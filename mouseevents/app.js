60e1637b13f6fa63b33a8d2aed4b5193284fa027
Harman <harmas7626@gmail.com>
2023-03-14 15:03:12 +0530

    first commit

---
+++ mouseevents/app.js
@@ -0,0 +1,20 @@
+// function hover(){
+//     console.log('mouse is hover to heading')
+// }
+
+// let alertdiv = document.getElementById('alert');
+// function mouseOver(){
+//     alertdiv.style.display = 'block'
+// }
+
+// function mouseOut(){
+//     alertdiv.style.display = 'none'
+// }
+
+
+function submit(){
+    let name = document.getElementById('name')
+    let value = name.value
+    document.getElementById('userinput').innerText = value
+    // name.value = ''
+}
