e26f6dd483a246cc6d7084c66f6fecc0a675af6c
Harman <harmas7626@gmail.com>
2023-03-14 22:05:08 +0530

    more js

--- mouseevents/app.js
+++ mouseevents/app.js
@@ -12,9 +12,36 @@
 // }
 
 
-function submit(){
-    let name = document.getElementById('name')
-    let value = name.value
-    document.getElementById('userinput').innerText = value
-    // name.value = ''
+// function submit(){
+    // let name = document.getElementById('name')
+//     let value = name.value
+//     document.getElementById('userinput').innerText = value
+//     // name.value = ''
+// }
+
+let magicstatus = false;
+let magicbtn = document.getElementById('magicbtn')
+let stopmagicbtn = document.getElementById('stopmagicbtn')
+
+function magic(){
+    magicstatus = true;
+    let magic = document.getElementById('title')
+    title.classList.add('title') 
+}
+
+function stopmagic(){
+    magicstatus = false;
+    let magic = document.getElementById('title')
+    title.classList.remove('title')  
+}
+
+function checkmagicstatus(){
+    if(magicstatus == true){
+        magicbtn.style.display = 'none';
+        stopmagicbtn.style.display = 'block'
+    }
+    else{
+        stopmagicbtn.style.display = 'none';
+        magicbtn.style.display = 'block'
+    }
 }
